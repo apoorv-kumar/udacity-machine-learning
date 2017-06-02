431c7b9e4e23742f0323608de86f766ae4b54a44
Apoorv Kumar <apoorv.kumar247@gmail.com>
2017-06-02 22:18:53 +0530

    encoders snippets

---
+++ tutorial_snippets/Encoding/encoder_basic.py
@@ -0,0 +1,58 @@
+from sklearn import preprocessing
+import pandas as pd
+sample_data = { 
+    'name' : ['Ray', 'Adam', 'Jason', 'Varun'],
+    'health' : ['fit', 'slim', 'obese', 'fit']
+ }
+
+data = pd.DataFrame(sample_data)
+
+print(data)
+
+label_encoder = preprocessing.LabelEncoder()
+
+label_encoder.fit(data['health'])
+
+# we could do multiple columns at once with pandas get dummies
+# see - http://fastml.com/converting-categorical-data-into-numbers-with-pandas-and-scikit-learn/
+
+encoded = label_encoder.transform(data['health'])
+print("label encoded:" + str(encoded))
+
+#   health   name
+# 0    fit    Ray
+# 1   slim   Adam
+# 2  obese  Jason
+# 3    fit  Varun
+# label encoded:[0 2 1 0]
+
+label_encoded_data = label_encoder.fit_transform(data['health'])
+# reshape tells fittransform that it's single feature array
+shaped_data = label_encoded_data.reshape(-1,1)
+print("shaped data:\n" + str(shaped_data))
+
+# shaped data:
+# [[0]
+#  [2]
+#  [1]
+#  [0]]
+
+ohe = preprocessing.OneHotEncoder(sparse=False) 
+ohe_encoded = ohe.fit_transform(shaped_data)
+print("ohe encoded (array representation):\n" + str(ohe_encoded))
+
+# ohe encoded (array representation):
+# [[ 1.  0.  0.]
+#  [ 0.  0.  1.]
+#  [ 0.  1.  0.]
+#  [ 1.  0.  0.]]
+
+ohe = preprocessing.OneHotEncoder() 
+ohe_encoded = ohe.fit_transform(shaped_data)
+print("ohe encoded (sparse matrix representation):\n" + str(ohe_encoded))
+
+# ohe encoded (sparse array representation):
+#   (0, 0)        1.0
+#   (1, 2)        1.0
+#   (2, 1)        1.0
+#   (3, 0)        1.0
---
+++ tutorial_snippets/Encoding/titanic.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+X = pd.read_csv("titanic_data.csv")
+X = X.dropna()
+X = X.select_dtypes(include=[object]) #string is an object
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+le = LabelEncoder()
+
+# NOTE : single column index returns Series
+#        multiple columns returns sliced DataFrame
+# We use dataframe here to make it clear to fit_transform
+# that it's a list of single feature and not single element
+# of n features
+# https://stackoverflow.com/questions/43475439/python-deprecationwarning-passing-1d-arrays-as-data-is-deprecated-in-0-17-and-w
+
+print(type(X['Name']))   #series
+print(type(X[['Name']])) #dataframe
+
+for feature in X:
+    X[feature] = le.fit_transform(X[[feature]])
+
+print(X)
+
+ohe = OneHotEncoder()
+
+for feature in X:
+    X[feature] = ohe.fit_transform(X[[feature]])
+
+print(X[:1])
